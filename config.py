cdfea899b4c5bf7676fdaae903245599dbf2a8d8
Adnaan Bheda <adnaan.bheda@gmail.com>
2020-08-27 01:23:51 +0530

    feat: jwt secret keys

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = f'sqlite:////{os.path.join(basedir, "db.sqlite")}'
+    JWT_SECRET = '123456789'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', None)
+    JWT_SECRET = os.environ.get('JWT_SECRET', None)
 
 
 class DevelopmentConfig(Config):
